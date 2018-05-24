978cb53fba4b692390f149841a1ece0d3b475ea4
Osta9 <ico_moo@hotmail.com>
2018-05-24 15:51:19 +0200

    opet naslovna

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,17 @@
 @section('content')
 <b-container>
     <b-row class="flex-center">
-        <h1 class="mb-5 text-nowrap">SUSTAV ZA TRANSFUZIOLOGIJU</h1>
+        <h1 class="mb-5 display-4 text-center">Sustav za transfuziologiju</h1>
         <b-row>
-            <b-col cols="10" md="7">
+            <b-col cols="12" sm="7" class="d-flex justify-content-center px-3">
                 <b-img src="/../img/blooddonation.jpg" fluid alt="Responsive image" />
             </b-col>
 
-            <b-col cols="10" md="5" class="d-flex justify-content-sm-center justify-content-end">
+            <b-col cols="12" sm="5" class="d-flex justify-content-center justify-content-md-end">
                 <b-img src="/../img/chart.png" fluid alt="Responsive image" />
             </b-col>
             <b-col cols="12">
-                  <p class="text-center mt-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente eius ad ullam harum corporis. Optio perspiciatis facere veniam eius, distinctio laboriosam magni at aliquid aperiam architecto molestias, debitis culpa! Porro, incidunt eligendi expedita eum mollitia officia similique totam itaque, aperiam nam modi sint tempore esse deserunt. Quae dicta, atque voluptates.</p>
+                  <p class="text-center mt-5 p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente eius ad ullam harum corporis. Optio perspiciatis facere veniam eius, distinctio laboriosam magni at aliquid aperiam architecto molestias, debitis culpa! Porro, incidunt eligendi expedita eum mollitia officia similique totam itaque, aperiam nam modi sint tempore esse deserunt. Quae dicta, atque voluptates.</p>
                    <hr class="hr-danger">
             </b-col>
         </b-row>
@@ -60,6 +60,14 @@
                 <b-badge variant="danger" pill>1</b-badge>
               </b-list-group-item>
             </b-list-group>
+            <b-row class="flex-center col-12">
+                <b-col cols="8">
+                    <hr class="hr-red mt-5">
+                </b-col>
+                <b-col cols=5>
+                <footer class="my-4 py-3 text-center">Sustav za transfuziologiju @ 2018.</footer>
+                </b-col>
+            </b-row>
         </b-row>
 
     </b-row>
